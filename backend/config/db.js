fcb2920b3d00cdafea8c131400592c0ef70212f1
AbbasSalim <abbassysalim1@gmail.com>
2025-04-05 23:26:25 -0500

    mongodb working addition

---
+++ backend/config/db.js
@@ -0,0 +1,23 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri = "mongodb+srv://bassalim03:hacka042025@hacka-04-2025.6n2xfnt.mongodb.net/?retryWrites=true&w=majority&appName=hacka-04-2025";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("✅ Connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
